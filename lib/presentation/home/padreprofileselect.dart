d82e29e2066da54ff9e5643b5c60dacbcccb0612
JaredKohai <jaredbastarracheas@gmail.com>
2024-03-14 09:34:57 -0600

    Niños listo

--- lib/presentation/home/padreprofileselect.dart
+++ lib/presentation/home/padreprofileselect.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pictodi2/presentation/home/ninopage.dart';
 
 class PadresPages extends StatelessWidget {
   final String nombre;
@@ -10,9 +11,11 @@ class PadresPages extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: FutureBuilder<DocumentSnapshot>(
-        future:
-            FirebaseFirestore.instance.collection('padres').doc(nombre).get(),
+      body: FutureBuilder<QuerySnapshot>(
+        future: FirebaseFirestore.instance
+            .collection('padres')
+            .where('nombre', isEqualTo: nombre)
+            .get(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(
@@ -20,25 +23,16 @@ class PadresPages extends StatelessWidget {
             );
           }
 
-          if (!snapshot.hasData || !snapshot.data!.exists) {
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
             return Center(
               child: Text('No se encontraron datos para el padre.'),
             );
           }
 
-          // Obtener los datos del padre
-          Map<String, dynamic> data =
-              snapshot.data!.data() as Map<String, dynamic>;
+          QueryDocumentSnapshot padreDoc = snapshot.data!.docs.first;
+          Map<String, dynamic> data = padreDoc.data() as Map<String, dynamic>;
           String nombrePadre = data['nombre'];
           List<dynamic> hijos = data['hijos'];
-
-          List<Widget> perfiles = [];
-
-          // Crear un perfil para cada hijo del padre
-          for (var hijo in hijos) {
-            perfiles.add(ProfileBox(
-                imageUrl: 'https://via.placeholder.com/150', name: hijo));
-          }
 
           return Center(
             child: Column(
@@ -52,12 +46,29 @@ class PadresPages extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 20.0),
-                GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10.0,
-                  mainAxisSpacing: 10.0,
-                  shrinkWrap: true,
-                  children: perfiles,
+                ProfileBox(
+                  imageUrl: 'https://via.placeholder.com/150',
+                  name: nombrePadre,
+                  size: 100.0,
+                  onPressed: () {
+                    _buscarDatosHijo(nombrePadre, context);
+                  },
+                ),
+                SizedBox(height: 20.0),
+                Wrap(
+                  alignment: WrapAlignment.start,
+                  spacing: 10.0,
+                  runSpacing: 10.0,
+                  children: hijos
+                      .map((nombreHijo) => ProfileBox(
+                            imageUrl: 'https://via.placeholder.com/150',
+                            name: nombreHijo,
+                            size: 100.0,
+                            onPressed: () {
+                              _buscarDatosHijo(nombreHijo, context);
+                            },
+                          ))
+                      .toList(),
                 ),
               ],
             ),
@@ -66,38 +77,75 @@ class PadresPages extends StatelessWidget {
       ),
     );
   }
+
+  Future<void> _buscarDatosHijo(String nombreHijo, BuildContext context) async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('niños')
+        .where('nombre', isEqualTo: nombreHijo)
+        .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      QueryDocumentSnapshot hijoDoc = querySnapshot.docs.first;
+      Map<String, dynamic> data = hijoDoc.data() as Map<String, dynamic>;
+
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => HomePageKid(
+            nombre: data['nombre'],
+            instituto: data['instituto'],
+            diagnostico: data['diagnostico'],
+            fecha_nacimiento: data['fecha_nacimiento'],
+            grado: data['grado'],
+            grupo: data['grupo'],
+            gravedad: data['gravedad'],
+          ),
+        ),
+      );
+    }
+  }
 }
 
 class ProfileBox extends StatelessWidget {
   final String imageUrl;
   final String name;
+  final double size;
+  final VoidCallback onPressed;
 
-  const ProfileBox({Key? key, required this.imageUrl, required this.name})
+  const ProfileBox(
+      {Key? key,
+      required this.imageUrl,
+      required this.name,
+      required this.size,
+      required this.onPressed})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: Colors.grey,
-        image: DecorationImage(
-          image: NetworkImage(imageUrl),
-          fit: BoxFit.cover,
-        ),
-      ),
-      margin: EdgeInsets.all(10.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text(
-            name,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.0,
-            ),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.grey,
+          image: DecorationImage(
+            image: NetworkImage(imageUrl),
+            fit: BoxFit.cover,
           ),
-        ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              name,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
